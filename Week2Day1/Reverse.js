eb5e0274b387cfbcff1ca8dcdf1298e71b385008
YaminiKrishna-rs <95932485+YaminiKrishna-rs@users.noreply.github.com>
2026-08-16 15:18:58 +0530

    Add files via upload

---
+++ Week2Day1/Reverse.js
@@ -0,0 +1,13 @@
+let word="Testleaf"
+let rev=""
+
+for(let i = word.length-1; i>=0; i--)
+{
+    rev = rev + word.charAt(i)
+}
+
+console.log(rev);
+
+
+
+
---
+++ Week2Day2/sumOfAllElementsInAnArray.js
@@ -0,0 +1,8 @@
+const arr =[1,1,1,1,1]
+let sum=0
+
+for(let i=0;i<=arr.length-1;i++)
+{
+    sum = sum+arr[i]
+}
+console.log(sum);
